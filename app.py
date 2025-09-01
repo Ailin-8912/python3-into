f0af0bbb0463d928d865df0d0098cc232c2b9f76
Ailin-8912 <fadhilmuhammad.8912@gmail.com>
2025-09-01 21:25:20 +0700

    tes

---
+++ app.py
@@ -0,0 +1,6 @@
+print("BELAJAR GIT DASAR")
+# git add [nama_file_yg_berubah]
+# git commit -m "isi pesan berubah"
+# git push -u origin main
+
+# --- cara tambah app.py
---
+++ app_1.py
@@ -0,0 +1,2 @@
+print("BELAJAR GIT DASAR")
+# git add [nama_file_yg_berubah]
